ef7681471b7969a68ad1fa12073dad006c0498c4
Daniel Fonseca Alves <dalraf@gmail.com>
2022-08-30 18:41:18 -0300

    Versão finalizada mais não testada

--- WindowsDesktopStart.py
+++ WindowsDesktopStart.py
@@ -1,12 +1,28 @@
 import subprocess
 import urllib.request
 from pathlib import Path
+from google_drive import download_install_google, download_install_google_zip
 
 
 # Parametros Chocolatey
 ps_script_url = "https://community.chocolatey.org/install.ps1"
 ps_script_name = "install_chocolatey.ps1"
 
+# Parametros Caixa
+caixa_google_id = "1u0JnYuLxxcOgMTU5iYwMR20SOwBq3vaB"
+caixa_file_name = "Setup.jar"
+
+# Parametros Adobe Air
+adobeair_google_id = "13fUuPTnwpzIoydnefw9bcdX2h5KbJb0N"
+adobeair_file_name = "AdobeAir.exe"
+
+# Parametros Sisbr2.0
+sisbr20_google_id = "13E-X5fZZrj2FMZDIcLWJ94c9DgTqUA3f"
+sisbr20_file_name = "Sisbr2.0.exe"
+
+# Parametros Citrix10
+citrix_google_id = "19o1eGqGL6xR1B9b3VYunea4zzYe3Heb9"
+citrix_file_name = "Citrix10.zip"
 
 path_local = Path(".")
 
@@ -30,13 +46,51 @@ try:
         "firefox",
         "spark",
         'jre8 -PackageParameters "/exclude:64" -y',
-        'teamviewer',
-        'anydesk.install',
-        'adobereader',
+        "teamviewer",
+        "anydesk.install",
+        "adobereader",
     ]
     for programa in choco_install_list:
         subprocess.call("choco install -y " + programa)
 except Exception as e:
     print(e.args[0])
+
+# Instalacao do Caixa
+caixa_file_path = path_local / caixa_file_name
+download_install_google(
+    "Caixa",
+    caixa_google_id,
+    caixa_file_name,
+    f"java -jar  {caixa_file_path}",
+)
+
+# Instalacao do Adobe Air
+adobeair_file_path = path_local / adobeair_file_name
+download_install_google(
+    "Adobe Air",
+    adobeair_google_id,
+    adobeair_file_name,
+    f"{adobeair_file_path}",
+)
+
+# Instalacao do Sisbr2.0
+sisbr20_file_path = path_local / sisbr20_file_name
+download_install_google(
+    "Sisbr 2.0",
+    sisbr20_google_id,
+    sisbr20_file_name,
+    f"{sisbr20_file_path}",
+)
+
+# Instalacao Citrix10
+citrix_file_path = path_local / citrix_file_name
+citrix_path_execution = path_local / 'Citrix10' / 'Versao 10.1' / 'PN_10_1.msi'
+download_install_google_zip(
+    "Citrix",
+    citrix_google_id,
+    citrix_file_path,
+    f'msiexec /i {citrix_path_execution}',
+)
+
 
 input("Instalação finalizada, pressione qualquer tecla para fechar...")
--- google_drive.py
+++ google_drive.py
@@ -1,4 +1,7 @@
 import requests
+from pathlib import Path
+import subprocess
+import zipfile
 
 def download_file_from_google_drive(self, id, destination):
     def get_confirm_token(response):
@@ -27,3 +30,25 @@ def download_file_from_google_drive(self, id, destination):
         response = session.get(URL, params=params, stream=True)
 
     save_response_content(response, destination)
+
+def download_install_google(nome, id, file_name, cmd):
+    path_local = Path(".")
+    print(f'Instalando {nome}...')
+    file_path = path_local / file_name
+    try:
+        download_file_from_google_drive(id, file_path)
+        subprocess.call(cmd, shell=True)
+    except Exception as e:
+        print(e.args[0])
+
+def download_install_google_zip(nome, id, file_name, cmd):
+    path_local = Path(".")
+    print(f'Instalando {nome}...')
+    file_path = path_local / file_name
+    try:
+        download_file_from_google_drive(id, file_path)
+        with zipfile.ZipFile(file_path, "r") as citrixzip:
+            citrixzip.extractall(path_local)
+        subprocess.call(cmd, shell=True)
+    except Exception as e:
+        print(e.args[0])
