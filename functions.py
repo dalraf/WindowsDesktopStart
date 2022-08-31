be25a14383d8e33d694260c98adf3bdc24c5e873
Daniel Fonseca Alves <dalraf@gmail.com>
2022-08-31 12:24:01 -0300

    Melhorias

--- functions.py
+++ functions.py
@@ -10,7 +10,7 @@ def execute(cmd):
     if platform.system() == 'Linux':
         print(cmd)
     else:
-        subprocess.call(cmd, shell=True)
+        subprocess.call(cmd, cwd=path_local, shell=True)
 
 
 def install_chocolatey(ps_script_url, ps_script_name):
@@ -67,20 +67,26 @@ def download_file_from_google_drive(id, destination):
 def download_install_google(nome, id, file_name, cmd):
     print(f"Instalando {nome}...")
     file_path = path_local / file_name
-    try:
-        download_file_from_google_drive(id, file_path)
-        execute(cmd)
-    except Exception as e:
-        print(e.args[0])
+    if file_path.exists():
+        print('Arquivo existente não executando download')
+    else:
+        try:
+            download_file_from_google_drive(id, file_path)
+            execute(cmd)
+        except Exception as e:
+            print(e.args[0])
 
 
 def download_install_google_zip(nome, id, file_name, cmd):
     print(f"Instalando {nome}...")
     file_path = path_local / file_name
-    try:
-        download_file_from_google_drive(id, file_path)
-        with zipfile.ZipFile(file_path, "r") as citrixzip:
-            citrixzip.extractall(path_local)
-        execute(cmd)
-    except Exception as e:
-        print(e.args[0])
+    if file_path.exists():
+        print('Arquivo existente não executando download')
+    else:
+        try:
+            download_file_from_google_drive(id, file_path)
+            with zipfile.ZipFile(file_path, "r") as citrixzip:
+                citrixzip.extractall(path_local)
+            execute(cmd)
+        except Exception as e:
+            print(e.args[0])
