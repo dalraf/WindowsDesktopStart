465c8dfa6f91411063f9e72c01ee3aed527eb888
Daniel Fonseca Alves <dalraf@gmail.com>
2022-08-31 15:21:00 -0300

    Adicionado com gdown

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import subprocess
 import zipfile
 from vars import path_local
 import platform
+import gdown
 
 def execute(cmd):
     if platform.system() == 'Linux':
@@ -13,32 +14,7 @@ def execute(cmd):
 
 
 def download_file_from_google_drive(id, destination):
-    def get_confirm_token(response):
-        for key, value in response.cookies.items():
-            if key.startswith("download_warning"):
-                return value
-        return None
-
-    def save_response_content(response, destination):
-        CHUNK_SIZE = 32768
-
-        with open(destination, "wb") as f:
-            for chunk in response.iter_content(CHUNK_SIZE):
-                if chunk:  # filter out keep-alive new chunks
-                    f.write(chunk)
-
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params={"id": id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {"id": id, "confirm": token}
-        response = session.get(URL, params=params, stream=True)
-
-    save_response_content(response, destination)
+    gdown.download(id=id, output=str(destination), quiet=False)
 
 
 def download_install_google(nome, id, file_name, cmd):
